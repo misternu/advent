82fd1fa92521f2b1c5e6c15459b307a07499fa7d
Timothy Meixell <misternu@gmail.com>
2016-12-19 03:14:52 -0600

    Clean 18 with proc

--- 18/runner.rb
+++ 18/runner.rb
@@ -8,18 +8,20 @@ def next_row(row)
   end
 end
 
-# Part 1
-rows = [input.split('')]
-39.times do
-  rows << next_row(rows.last)
-end
-p rows.flatten.count('.')
-
-# Part 2
-count = input.count('.')
 new_row = input.split('')
-399999.times do
+count = new_row.count('.')
+
+gen_row = Proc.new do
   new_row = next_row(new_row)
   count += new_row.count('.')
 end
+
+# Part 1
+rows1 = 40
+(rows1 - 1).times { gen_row.call }
+p count
+
+# Part 2
+rows2 = 400000
+(rows2 - rows1).times { gen_row.call }
 p count
