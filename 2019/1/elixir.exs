de9f6cd64fe75f5ff8e6926211c40d13a547435d
Manu Meixell <manu@Manus-MacBook-Pro.local>
2019-12-01 00:02:07 -0600

    clean up elixir solution for 2019 day 1

--- 2019/1/elixir.exs
+++ 2019/1/elixir.exs
@@ -1,18 +1,11 @@
 defmodule Day1 do
   def fuel(modules) do
     modules
-    |> Enum.map(fn line ->
-      {integer, _leftover} = Integer.parse(line)
-      integer
-    end)
     |> Enum.map(fn number -> (div number, 3) - 2 end)
     |> Enum.sum
   end
 
-  def fuel_recursive(total, 0) do
-    total
-  end
-
+  def fuel_recursive(total, 0), do: total
   def fuel_recursive(total, module) do
     new_fuel = max((div module, 3) - 2, 0)
     fuel_recursive(total + new_fuel, new_fuel)
@@ -20,30 +13,21 @@ defmodule Day1 do
 
   def fuel_and_extra(modules) do
     modules
-    |> Enum.map(fn line ->
-      {integer, _leftover} = Integer.parse(line)
-      integer
-    end)
     |> Enum.map(fn number -> fuel_recursive(0, number) end)
     |> Enum.sum
   end
 end
 
-case System.argv do
-  [input_file] ->
-    __DIR__ <> "/" <> input_file
-    |> File.read!()
-    |> String.split("\n", trim: true)
-    |> Day1.fuel()
-    |> IO.inspect
+modules = __DIR__ <> "/" <> "input.txt"
+|> File.read!()
+|> String.split("\n", trim: true)
+|> Enum.map(fn line ->
+  {integer, _leftover} = Integer.parse(line)
+  integer
+end)
 
-    __DIR__ <> "/" <> input_file
-    |> File.read!()
-    |> String.split("\n", trim: true)
-    |> Day1.fuel_and_extra()
-    |> IO.inspect
+Day1.fuel(modules)
+|> IO.inspect
 
-  _ ->
-    IO.puts :stderr, "use --test or file name"
-    System.halt(1)
-end
+Day1.fuel_and_extra(modules)
+|> IO.inspect
