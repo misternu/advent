5165b1d8e66606572f1b4cc7a3d2de099b690cf9
Manu Meixell <misternu@gmail.com>
2019-12-10 14:17:29 -0600

    add slow elixir solution for 2019 day 3

--- 2019/03/elixir.exs
+++ 2019/03/elixir.exs
@@ -1,32 +1,35 @@
 defmodule Wire do
-  # def get_crumbs("U", position, distance) do
+  def add_crumb(crumbs, k, v), do: Map.get(crumbs, k) && crumbs || Map.put(crumbs, k, v)
 
-  # end
-
-  def get_crumbs("R", position, distance) do
-    {x, y} = position
-    Enum.map(1..distance, fn offset ->
-      {x + offset, y}
-    end)
+  def crumbs(br, "U", {x, y}, d, s) do
+    Enum.reduce(1..d, br, fn o, a -> add_crumb(a, {x, y + o}, s + o) end)
   end
 
-  # def get_crumbs("D", position, distance) do
-  # end
+  def crumbs(br, "R", {x, y}, d, s) do
+    Enum.reduce(1..d, br, fn o, a -> add_crumb(a, {x + o, y}, s + o) end)
+  end
 
-  # def get_crumbs("L", position, distance) do
-  # end
+  def crumbs(br, "D", {x, y}, d, s) do
+    Enum.reduce(1..d, br, fn o, a -> add_crumb(a, {x, y - o}, s + o) end)
+  end
 
-  def walk([], breadcrumbs, _position), do: breadcrumbs
-  def walk([instruction | tail], breadcrumbs, position) do
+  def crumbs(br, "L", {x, y}, d, s) do
+    Enum.reduce(1..d, br, fn o, a -> add_crumb(a, {x - o, y}, s + o) end)
+  end
+
+  def walk([], breadcrumbs, _position, _steps), do: breadcrumbs
+  def walk([instruction | tail], breadcrumbs, position, steps) do
     direction = String.at(instruction, 0)
     distance = String.slice(instruction, 1..-1)
       |> Integer.parse()
       |> elem(0)
-    get_crumbs(direction, position, distance) |> List.last |> IO.inspect
+    new_crumbs = crumbs(breadcrumbs, direction, position, distance, steps)
+    {new_position, new_steps} = Map.to_list(new_crumbs) |> Enum.max_by(fn {_p, s} -> s end)
+    walk(tail, Map.merge(new_crumbs, breadcrumbs), new_position, new_steps)
   end
 
   def walk(instructions) do
-    walk(instructions, [], {0,0})
+    walk(instructions, %{}, {0,0}, 0)
   end 
 end
 
@@ -37,5 +40,17 @@ end
     String.split(line, ",", trim: true)
   end)
 
+[wire1, wire2] = [Wire.walk(input1), Wire.walk(input2)]
 # Part 1
-Wire.walk(input1)
+[wire1points, wire2points] = [Map.keys(wire1), Map.keys(wire2)]
+overlap = wire1points -- (wire1points -- wire2points)
+overlap
+  |> Enum.map(fn {x, y} -> abs(x) + abs(y) end)
+  |> Enum.min
+  |> IO.puts
+
+# Part 2
+Enum.map(overlap, fn i ->
+  Map.get(wire1, i) + Map.get(wire2, i)
+end)
+  |> Enum.min |> IO.puts
