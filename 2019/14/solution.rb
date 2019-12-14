27b1820effc424f75a3080642d2136b3ed59a5d0
Manu Meixell <misternu@gmail.com>
2019-12-14 16:53:37 -0600

    add solution for 2019 day 14

---
+++ 2019/14/solution.rb
@@ -0,0 +1,33 @@
+require_relative '../../lib/advent_helper'
+
+helper = AdventHelper.new(script_root:__dir__)
+
+input = helper.line_separated_strings('input.txt')
+
+reactions = Hash.new
+input.each do |reaction|
+  ingredients, output = reaction.split('=>').map(&:strip)
+  output = output.split(' ').tap { |o| o[0] = o[0].to_i}
+  ingredients = ingredients.split(', ').map { |i| i.split(' ') }.map { |i| [i[0].to_i, i[1]] }
+  reactions[output] = ingredients
+end
+
+def ore_need(reactions, n)
+  needs = Hash.new(0)
+  needs["FUEL"] = n
+  until needs.all? { |k, v| k == "ORE" || v <= 0 }
+    key = needs.find { |k, v| k != "ORE"  && v > 0 }.first
+    reaction = reactions.keys.find { |n, i| n > 0 && i == key }
+    times = (needs[key].to_f / reaction.first).ceil 
+    reactions[reaction].each { |i| needs[i.last] += i.first * times }
+    needs[reaction.last] -= reaction.first * times
+  end
+  needs["ORE"]
+end
+
+# Part 1
+p ore_need(reactions, 1)
+
+ONE_TRILLION = 1000000000000
+# Part 2
+p (0..ONE_TRILLION).bsearch { |n| ore_need(reactions, n) > ONE_TRILLION } - 1
