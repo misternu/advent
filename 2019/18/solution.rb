735e1dfde5cbbb45aed295d15966cd4f6795577a
Manu Meixell <misternu@gmail.com>
2020-01-06 11:28:38 -0600

    Add working solution to 2019 day 18

--- 2019/18/solution.rb
+++ 2019/18/solution.rb
@@ -34,85 +34,53 @@ class PriorityQueue
   end
 end
 
-class AStar
-  def initialize(map, goal, origin = [0, 0])
-    @map = map
-    @queue = PriorityQueue.new
-    @goal = goal
-    @origin = origin
-  end
-
-  def path
-    @queue.add(0, @origin)
-    came_from = Hash.new
-    cost_so_far = Hash.new
-    cost_so_far[@origin] = 0
-    while @queue.any?
-      current = @queue.next
-      break if current == @goal
-      neighbors(current).each do |n|
-        new_cost = cost_so_far[current] + 1
-        if !cost_so_far[n] || new_cost < cost_so_far[n]
-          cost_so_far[n] = new_cost
-          priority = new_cost + manhattan(n)
-          @queue.add(priority, n)
-          came_from[n] = current
-        end
-      end
-    end
-    if came_from[@goal]
-      current = @goal
-      path = []
-      while current
-        path << current
-        current = came_from[current]
-      end
-      return path.reverse
-    end
-    nil
-  end
-
-  def manhattan(pos)
-    (@origin.first - pos.first).abs + (@origin.last - pos.last).abs
-  end
-
-  def neighbors(pos)
-    [up(pos), down(pos), left(pos), right(pos)].select { |p| p == @goal || @map[p] == "." }
-  end
-end
-
 def neighbors(map, pos)
   [up(pos), down(pos), left(pos), right(pos)].reject { |p| map[p].nil? || map[p] == "#"  }
 end
 
-def options(map, start, keys = [])
+def options(map, start, keys)
   result = []
   came_from = Hash.new
+  distance = Hash.new
+  distance[start] = 0
   queue = [start]
   while queue.any?
     current = queue.shift
     neighbors(map, current).each do |n|
       next if came_from[n]
-      if map[n] == "."
-        came_from[n] = current
-        queue << n
-      else
-        result << map[n]
+      char = map[n]
+      unless char == "."
+        if !!/[[:upper:]]/.match(char)
+          next unless keys.include?(char.downcase)
+        elsif !keys.include?(char)
+          result << [map[n], distance[current] + 1, n]
+          next
+        end
       end
+      came_from[n] = current
+      distance[n] = distance[current] + 1
+      queue << n
     end
   end
-  result.select { |c| !!/[[:lower:]]/.match(c) }
+  result.select { |c| !!/[[:lower:]]/.match(c.first) }
+end
+
+def minimum_steps(memo, map, start, keys)
+  keystring = keys.join
+  return memo[[start, keystring]] if memo[[start, keystring]]
+  opts = options(map, start, keys)
+  if opts.length == 0
+    memo[[start, keystring]] = 0
+    return 0
+  end
+  distances = []
+  opts.each do |key, distance, position|
+    distances << distance + minimum_steps(memo, map, position, (keys + [key]).sort)
+  end
+  memo[[start, keystring]] = distances.min
 end
 
 input = helper.line_separated_strings('input.txt')
-
-# input = [
-#   "########################",
-#   "#...............b.C.D.f#",
-#   "#.######################",
-#   "#.....@.a.B.c.d.A.e.F.g#",
-#   "########################"
-# ]
 
 grid = Hash.new
 
@@ -122,48 +90,48 @@ input.each_with_index do |row, y|
   end
 end
 
-keys = grid.select { |k, v| !!/[[:lower:]]/.match(v) }
-keys_len = keys.length
-doors = grid.select { |k, v| !!/[[:upper:]]/.match(v) }
 start_pos = grid.find { |k, v| v == "@" }.first
 grid[start_pos] = "."
 
-# Part 1
-# { map: map, pos: pos, steps: x, keys:["a", "b"] }
-
-shortest_so_far = Float::INFINITY
-queue = PriorityQueue.new
-queue.add(0, {map: grid.dup, pos: start_pos, steps: 0, keys: []})
-while queue.any?
-  p queue.length
-  current = queue.next
-  if current[:keys].length == keys_len
-    p "FOUND"
-    p shortest_so_far = [shortest_so_far, current[:steps]].min
-    next
-  end
-  if current[:steps] > shortest_so_far
-    next
-  end
-  options(current[:map], current[:pos]).each do |option|
-    new_pos = keys.key(option)
-    astar = AStar.new(current[:map], new_pos, current[:pos])
-    steps = astar.path.length - 1
-    new_map = current[:map].dup
-    new_map[new_pos] = "."
-    new_map[doors.key(option.capitalize)] = "."
-    new_state = {
-      map: new_map,
-      pos: new_pos,
-      steps: current[:steps] + steps,
-      keys: current[:keys] + [option]
-    }
-    queue.add([keys_len - new_state[:keys].length, new_state[:steps]], new_state)
-  end
-end
-p shortest_so_far
-
-# paths:
+# >5 minutes
+# p minimum_steps(Hash.new, grid, start_pos, [])
 
 # Part 2
-input
+def minimum_steps_multiple(memo, map, starts, keys)
+  keystring = keys.join
+  return memo[[starts, keystring]] if memo[[starts, keystring]]
+  opts = starts.map { |start| options(map, start, keys) }
+  if opts.map { |o| o.length }.reduce(&:+) == 0
+    memo[[starts, keystring]] = 0
+    return 0
+  end
+  distances = []
+  opts.each_with_index do |bot, i|
+    bot.each do |key, distance, position|
+      new_starts = starts.dup
+      new_starts[i] = position
+      distances << distance + minimum_steps_multiple(
+        memo,
+        map,
+        new_starts,
+        (keys + [key]).sort
+      )
+    end
+  end
+  memo[[starts, keystring]] = distances.min
+end
+
+grid[up(start_pos)] = "#"
+grid[down(start_pos)] = "#"
+grid[left(start_pos)] = "#"
+grid[right(start_pos)] = "#"
+grid[start_pos] = "#"
+start_pos = [
+  [start_pos.first - 1, start_pos.last - 1],
+  [start_pos.first + 1, start_pos.last - 1],
+  [start_pos.first - 1, start_pos.last + 1],
+  [start_pos.first + 1, start_pos.last + 1]
+]
+
+# current time 14+ minutes
+p minimum_steps_multiple(Hash.new, grid, start_pos, [])
