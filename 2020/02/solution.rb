404e5b6c2f54aa3316bde77714326aeb72e18f93
Manu Meixell <misternu@gmail.com>
2020-12-01 23:38:33 -0600

    Add solution for 2020 day 2

--- 2020/02/solution.rb
+++ 2020/02/solution.rb
@@ -1,15 +1,32 @@
 require_relative '../../lib/advent_helper'
 helper = AdventHelper.new(script_root:__dir__)
 input = helper.line_separated_strings('input.txt')
-# input = helper.comma_separated_strings('input.txt')
 
 
 
 # Part 1
-a = input.dup
+re = /(\d+)-(\d+) (.): (\S+)$/
+a = input.count do |value|
+  matches = re.match(value)[1..-1]
+  low = matches[0].to_i
+  high = matches[1].to_i
+  char = matches[2]
+  password = matches[3]
+
+  repetition = password.split('').count { |letter| letter == char }
+  repetition >= low && repetition <= high
+end
 
 # Part 2
-b = nil
+b = input.count do |value|
+  matches = re.match(value)[1..-1]
+  low = matches[0].to_i
+  high = matches[1].to_i
+  char = matches[2]
+  password = matches[3]
+
+  (password[low - 1] == char) != (password[high - 1] == char)
+end
 
 
 
--- lib/template.rb
+++ lib/template.rb
@@ -1,8 +1,9 @@
 require_relative '../../lib/advent_helper'
-
 helper = AdventHelper.new(script_root:__dir__)
-
 input = helper.line_separated_strings('input.txt')
+# input = helper.comma_separated_strings('input.txt')
+
+
 
 # Part 1
 a = input.dup
@@ -10,3 +11,6 @@ a = input.dup
 # Part 2
 b = nil
 
+
+
+helper.output(a, b)
