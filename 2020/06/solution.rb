e1e03fdcd1294b139de4a5f04698e624ea6bef24
Manu Meixell <misternu@gmail.com>
2020-12-05 23:52:40 -0600

    Refactor 2020 day 6

--- 2020/06/solution.rb
+++ 2020/06/solution.rb
@@ -2,20 +2,15 @@ require_relative '../../lib/advent_helper'
 helper = AdventHelper.new(script_root:__dir__)
 input = helper.send(:open_file, 'input.txt').read
 responses = input.split("\n\n")
-responses.map! { |response| response.split(/\s+/) }
+responses.map! { |r| r.split(/\s+/).map(&:chars) }
 
 
 
 # Part 1
-a = responses.map do |response|
-  response.map { |person| person.split('').uniq }.flatten.uniq.count
-end .sum
+a = responses.sum { |r| r.inject(&:|).count }
 
 # Part 2
-b = responses.map do |response|
-  people = response.map { |person| person.split('').uniq }
-  people[1..-1].inject(people.first) { |array, p| array & p } .length
-end .sum
+b = responses.sum { |r| r.inject(&:&).count }
 
 
 
