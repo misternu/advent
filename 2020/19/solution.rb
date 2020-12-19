326e72d6e89d3c547406705984a47822276ef641
Manu Meixell <misternu@gmail.com>
2020-12-19 11:15:26 -0600

    refactor 2020 day 19

--- 2020/19/solution.rb
+++ 2020/19/solution.rb
@@ -10,83 +10,40 @@ input_rules, input_messages = input.split("\n\n").map { |part| part.split("\n") 
 
 
 
-# Part 1
 rules = Hash.new
 input_rules.each do |rule|
   if rule.include?("\"")
-    i, letter = rule.split(/\W+/)
-    rules[i.to_i] = letter
-  elsif rule.include?("|")
-    i, values = rule.split(":")
-    a, b = values.split("|").map { |v| v.strip.split(/\W+/).map(&:to_i) }
-    rules[i.to_i] = [a, b]
+    i, value = rule.split(/\W+/)
   else
-    i, *nums = rule.split(/\W+/).map(&:to_i)
-    rules[i] = nums
-    rules[i]
+    i, variants = rule.split(":")
+    value = variants.split("|").map { |v| v.strip.split(/\W+/).map(&:to_i) }
   end
+  rules[i.to_i] = value
 end
 
-def get_strings(rules, value)
-  if value.first.is_a?(Array)
-    return get_strings(rules, value.first) + get_strings(rules, value.last)
-  end
-  result = value.map do |i|
-    rule = rules[i]
-    if ["a", "b"].include?(rule)
-      [rule]
-    else
-      get_strings(rules, rule)
-    end
-  end
-  result.reduce { |acc, v| acc.product(v).map(&:join) }
-end
-
-viable_strings = Hash[get_strings(rules, [0]).collect { |i| [i, true] }]
-
-a = input_messages.count do |message|
-  viable_strings[message]
-end
-
-
-# Part 2
-# 8: 42 | 42 8
-# 11: 42 31 | 42 11 31
-# p fourty_two = get_strings(rules, [42]).uniq
-# p thirty_one = get_strings(rules, [31]).uniq
-
-def search(rules, string, rule_id, values)
-  # p [string, values]
+def search(rules, string, rule_id = 0, values = [])
   rule = rules[rule_id]
   if rule.is_a?(String)
-    if values.empty?
-      string == rule
-    else
-      string[0] == rule && search(rules, string[1..-1], values[0], values[1..-1])
-    end
-  elsif rule.first.is_a?(Array)
-    rule.any? do |variant|
-      new_values = variant + values
-      search(rules, string, new_values[0], new_values[1..-1])
-    end
-  else
-    new_values = rule + values
+    return string == rule if values.empty?
+    return string[0] == rule && search(rules, string[1..-1], values[0], values[1..-1])
+  end
+  rule.any? do |variant|
+    new_values = variant + values
     search(rules, string, new_values[0], new_values[1..-1])
   end
 end
 
+# Part 1
+a = input_messages.count { |message| search(rules, message) }
+
+# Part 2
+# 8: 42 | 42 8
+# 11: 42 31 | 42 11 31
+
 rules[8] = [[42], [42,8]]
 rules[11] = [[42,31], [42,11,31]]
 
-b = input_messages.count do |message|
-  value = search(rules, message, 0, [])
-  if value
-    p message
-  end
-  !!value
-end
-# b = nil
-# b = search(rules, "abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa", 0, [])
+b = input_messages.count { |message| search(rules, message) }
 
 
 helper.output(a, b)
