46e29990d46ebb16717b454e645599532d8ace37
Manu Meixell <misternu@gmail.com>
2020-12-25 00:24:44 -0600

    Add 2020 day 25 solution

---
+++ 2020/25/solution.rb
@@ -0,0 +1,33 @@
+require_relative '../../lib/advent_helper'
+helper = AdventHelper.new(script_root:__dir__, script_file: __FILE__)
+input = helper.auto_parse
+
+
+def solve(key)
+  count = 0
+  memo = 1
+  while true
+    return count if memo == key
+    memo = (memo * 7) % 20201227
+    count += 1
+  end
+end
+
+def encryption(subject, loop_size)
+  (subject ** loop_size) % 20201227
+end
+
+def encryption_larger_loop_size(subject, loop_size)
+  (1..loop_size).reduce(1) { |memo, l| memo * subject % 20201227 }
+end
+
+card_public, door_public = input
+
+card_loops = solve(card_public)
+door_loops = solve(door_public)
+
+a = encryption(door_public, card_loops)
+b = encryption_larger_loop_size(card_public, door_loops)
+
+
+helper.output(a, b)
--- lib/advent_helper.rb
+++ lib/advent_helper.rb
@@ -2,6 +2,7 @@ require_relative './csv_helpers'
 require_relative './print_helpers'
 require_relative './auto_parser'
 require_relative './overrides'
+require 'yaml'
 
 class AdventHelper
   include CSVHelpers
@@ -9,13 +10,23 @@ class AdventHelper
 
   def initialize(options = {})
     @script_root = options.fetch(:script_root, root_dir)
+    @script_file = options.fetch(:script_file, '')
     clear if options.fetch(:clear, true)
+    load_config
     start_counter
+  end
+
+  def load_config
+    if File.exists?(File.join(root_dir, '.advent_config.yml'))
+      @config = YAML.load_file(File.join(root_dir, '.advent_config.yml'))
+    else
+      @config = {}
+    end
   end
 
   def start_counter
     print "\e[2J"
-    print "Running #{__FILE__}...\n"
+    print "Running #{@script_file}...\n"
     @counter = Thread.new {
       start = Time.now
       while true do
