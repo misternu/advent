a36fd87119bff3f722a47ac438e55ba11d37e5f2
Manu Meixell <manu@Manus-MacBook-Pro.local>
2021-11-28 16:58:45 -0700

    2012/22 wip 2

--- 2019/22/instruction_parser.rb
+++ 2019/22/instruction_parser.rb
@@ -22,11 +22,18 @@ class InstructionParser
   end
 
   def compress
-    parse.reduce do |a, b|
-      a_i, a_o = a
-      b_i, b_o = b
-      [a_i * b_i % @length, ((a_o * b_i) + b_o) % @length]
+    parse.reduce(&compression_proc)
+  end
+
+  def repeat(n)
+    pws = powers_of_2(n)
+    highest = pws.last
+    hash = { 1 => compress }
+    (2..highest).each do |m|
+      lower = hash[m-1]
+      hash[m] = compression_proc.call(lower, lower)
     end
+    pws.map { |m| hash[m] } .reduce(&compression_proc)
   end
 
   def self.parse(instructions, options = {})
@@ -35,5 +42,32 @@ class InstructionParser
 
   def self.compress(instructions, options = {})
     new(instructions, options).compress
+  end
+
+  def self.repeat(instructions, repetitions, options = {})
+    new(instructions, options).repeat(repetitions)
+  end
+
+  private
+
+  def compression_proc
+    Proc.new do |a, b|
+      a_i, a_o = a
+      b_i, b_o = b
+      [a_i * b_i % @length, ((a_o * b_i) + b_o) % @length]
+    end
+  end
+
+  def powers_of_2(n)
+    pws = []
+    x = 1
+    until n == 0
+      unless (n % 2 == 0)
+        pws << x
+      end
+      n /= 2
+      x += 1
+    end
+    pws
   end
 end
--- 2019/22/solution_3.rb
+++ 2019/22/solution_3.rb
@@ -16,8 +16,12 @@ deck.apply_all(instructions)
 a = deck.index_of(n)
 
 # part 2
-# length = 119315717514047
-# repetitions = 101741582076661
-# i = 2020
+length = 119315717514047
+repetitions = 101741582076661
+i = 2020
+
+increment, offset = InstructionParser.repeat(input, repetitions, length: length)
+deck = Deck.new(length, increment: increment, offset: offset)
+
 
 helper.output(a, nil)
