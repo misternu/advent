e3cdc5749628a24e921d3e8ff2857ac15b4cfa84
Manu Meixell <misternu@gmail.com>
2021-12-01 22:25:20 -0700

    clean up 2021/02

--- 2021/02/solution.rb
+++ 2021/02/solution.rb
@@ -1,17 +1,17 @@
 require_relative '../../lib/advent_helper'
 helper = AdventHelper.new(script_root:__dir__)
 input = helper.auto_parse
+pairs = input.map { |a, b| [a, b.to_i] }
 
 # Part 1
-pairs = input.map { |a, b| [a, b.to_i] }
 pos = [0,0]
 
 pairs.each do |dir, n|
   case dir
   when 'up'
-    pos[1] += n
-  when 'down'
     pos[1] -= n
+  when 'down'
+    pos[1] += n
   when 'forward'
     pos[0] += n
   end
@@ -20,13 +20,10 @@ end
 a = pos[0] * pos[1]
 
 # Part 2
-
 pos = [0,0]
 aim = 0
 
 pairs.each do |dir, n|
-  p pos
-  p aim
   case dir
   when 'up'
     # pos[1] += n
