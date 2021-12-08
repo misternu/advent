4238eba06e949972dfa78b7d8fb4705520cdc7d8
Manu Meixell <misternu@gmail.com>
2021-12-07 21:14:35 -0700

    optimize memory for 2021/07

--- 2021/07/solution.rb
+++ 2021/07/solution.rb
@@ -1,22 +1,32 @@
 require_relative '../../lib/advent_helper'
+require 'memory_profiler'
 helper = AdventHelper.new(script_root:__dir__)
 input = helper.auto_parse
 sample_input = "16,1,2,0,4,2,7,1,2,14".split(',').map(&:to_i)
 # input = sample_input
+# MemoryProfiler.start(allow_files: __FILE__)
 
 
 # Part 1
 min = input.min
 max = input.max
 
-a = (min..max).map { |i| input.map { |j| (i - j).abs }.sum }.min
+a = (min..max).reduce(Float::INFINITY) do |m, i|
+  f = input.sum { |j| (i - j).abs }
+  f < m ? f : m
+end
 
 # Part 2
 def fuel(n)
   (n * (n + 1)) / 2
 end
-b = (min..max).map { |i| input.map { |j| fuel((i - j).abs) }.sum }.min
+b = (min..max).reduce(Float::INFINITY) do |m, i|
+  f = input.sum { |j| fuel((i - j).abs) }
+  f < m ? f : m
+end
 
 
+# MemoryProfiler.stop.pretty_print
 
 helper.output(a, b)
+
--- lib/template.rb
+++ lib/template.rb
@@ -3,7 +3,7 @@ helper = AdventHelper.new(script_root:__dir__)
 # input = helper.send(:open_file, 'input.txt').read
 # input = helper.line_separated_strings('input.txt')
 input = helper.auto_parse
-
+# MemoryProfiler.start(allow_files: __FILE__)
 
 
 # Part 1
@@ -13,5 +13,6 @@ a = input.dup
 b = nil
 
 
+# MemoryProfiler.stop.pretty_print
 
 helper.output(a, b)
