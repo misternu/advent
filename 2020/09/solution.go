fa0c121a1941b823a3acd5cf4675a33a25f84e4a
Manu Meixell <misternu@gmail.com>
2022-02-20 14:57:45 -0600

    2020 day 9 golang solution

--- 2020/09/solution.go
+++ 2020/09/solution.go
@@ -8,9 +8,11 @@ import (
   "strings"
 )
 
-func readInput() ([]int) {
+func readInput() []int {
   input, err := ioutil.ReadFile("2020/09/input.txt")
-  if err != nil { panic(err) }
+  if err != nil {
+    panic(err)
+  }
 
   reader := strings.NewReader(string(input))
   scanner := bufio.NewScanner(reader)
@@ -24,16 +26,64 @@ func readInput() ([]int) {
   return result
 }
 
-func partOne(numbers) {
-  length = len(numbers)
+func partOne(numbers []int) int {
+  length := len(numbers)
 
   for i := 25; i < length; i++ {
-
+    var sums []int
+    for j := i - 25; j < i-1; j++ {
+      for k := j + 1; k < i; k++ {
+        sums = append(sums, numbers[j]+numbers[k])
+      }
+    }
+    valid := false
+    for _, sum := range sums {
+      if numbers[i] == sum {
+        valid = true
+        break
+      }
+    }
+    if !valid {
+      return numbers[i]
+    }
   }
+
+  return -1
+}
+
+func partTwo(numbers []int, target int) int {
+  i := 0
+  j := 1
+  sum := numbers[i] + numbers[j]
+  for sum != target {
+    if sum < target {
+      j++
+      sum = sum + numbers[j]
+    } else {
+      sum = sum - numbers[i]
+      i++
+    }
+  }
+  min := numbers[i]
+  max := numbers[j]
+  for k := i; k <= j; k++ {
+    number := numbers[k]
+    if number < min {
+      min = number
+    }
+    if number > max {
+      max = number
+    }
+  }
+  return min + max
 }
 
 func main() {
   numbers := readInput()
 
-  partOne(numbers)
+  part_one := partOne(numbers)
+  part_two := partTwo(numbers, part_one)
+
+  fmt.Printf("%d\n", part_one)
+  fmt.Printf("%d\n", part_two)
 }
