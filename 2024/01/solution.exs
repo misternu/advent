b803d00de8882d46e19a5038c96c2285b7da38f3
Manu Meixell <misternu@gmail.com>
2024-12-01 10:43:26 -0700

    2024 day 1 elixir solution

--- 2024/01/solution.exs
+++ 2024/01/solution.exs
@@ -1,6 +1,26 @@
 defmodule Solution do
-  def run do
+  def part_one(left, right) do
+    Enum.zip(Enum.sort(left), Enum.sort(right))
+      |> Enum.map(fn {a, b} -> abs(a - b) end)
+      |> Enum.sum
+      |> IO.inspect
+  end
 
+  def part_two(left, right) do
+    Enum.map(left, fn a -> Enum.count(right, fn b -> a == b end) * a end)
+      |> Enum.sum
+      |> IO.inspect
+  end
+
+  def run do
+    [left, right] = __DIR__ <> "/" <> "input.txt"
+      |> File.read!()
+      |> String.trim()
+      |> String.split("\n")
+      |> Enum.map(fn s -> Enum.map(String.split(s, "   "), &String.to_integer/1) end)
+      |> Enum.zip_with(&Function.identity/1)
+    part_one(left, right)
+    part_two(left, right)
   end
 end
 
