e11e0c26eef2274c653d5e2d96d170c5b3439349
Manu Meixell <misternu@gmail.com>
2024-12-01 21:19:03 -0700

    2024 day 02 blank

---
+++ 2024/02/solution.rb
@@ -0,0 +1,23 @@
+require_relative '../../lib/advent_helper'
+# require 'memory_profiler'
+helper = AdventHelper.new(script_root: __dir__, counter: false)
+# input = helper.send(:open_file, 'input.txt').read
+# input = helper.line_separated_strings('input.txt')
+# input = helper.comma_separated_strings('input.txt')
+input = helper.auto_parse
+# sample_input = helper.line_separated_strings('sample_input.txt')
+sample_input = helper.auto_parse('sample_input.txt')
+# MemoryProfiler.start(allow_files: __FILE__)
+
+# input = sample_input
+
+# Part 1
+a = input.dup
+
+# Part 2
+b = nil
+
+
+
+# MemoryProfiler.stop.pretty_print
+helper.output(a, b)
