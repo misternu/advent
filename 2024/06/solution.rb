eea99e898664e3edecda015fad0c5b42e49d2f33
Manu Meixell <misternu@gmail.com>
2024-12-05 22:15:15 -0800

    2024 day 6 solution

--- 2024/06/solution.rb
+++ 2024/06/solution.rb
@@ -2,20 +2,73 @@ require_relative '../../lib/advent_helper'
 # require 'memory_profiler'
 helper = AdventHelper.new(script_root: __dir__, counter: false)
 # input = helper.send(:open_file, 'input.txt').read
-# input = helper.line_separated_strings('input.txt')
+input = helper.line_separated_strings('input.txt')
 # input = helper.comma_separated_strings('input.txt')
-input = helper.auto_parse
-# sample_input = helper.line_separated_strings('sample_input.txt')
-sample_input = helper.auto_parse('sample_input.txt')
+# input = helper.auto_parse
+sample_input = helper.line_separated_strings('sample_input.txt')
+# sample_input = helper.auto_parse('sample_input.txt')
 # MemoryProfiler.start(allow_files: __FILE__)
 
 # input = sample_input
 
 # Part 1
-a = input.dup
+row_count = input.length
+col_count = input[0].length
+obstacles = {}
+input_pos = nil
+
+input.each_with_index do |row, y|
+  row.split('').each_with_index do |col, x|
+    if col == "^"
+      input_pos = [x, y]
+    elsif col == "#"
+      obstacles[[x, y]] = true
+    end
+  end
+end
+
+dir = 0
+pos = input_pos.dup
+DIRS = [[0, -1], [1, 0], [0, 1], [-1, 0]].freeze
+visited = {}
+
+while pos[0] >= 0 && pos[1] >= 0 && pos[0] < col_count && pos[1] < row_count
+  new_pos = [pos[0] + DIRS[dir][0], pos[1] + DIRS[dir][1]]
+  if obstacles[new_pos]
+    dir = (dir + 1) % 4
+  else
+    visited[pos] = true
+    pos = new_pos
+  end
+end
+
+a = visited.keys.count
 
 # Part 2
-b = nil
+
+b = 0
+
+visited.keys.each do |obst|
+  next if obstacles[obst]
+  next if input_pos == obst
+
+  pos = input_pos.dup
+  dir = 0
+  visited = {}
+  while pos[0] >= 0 && pos[1] >= 0 && pos[0] < col_count && pos[1] < row_count
+    if visited[(pos + [dir])]
+      b += 1
+      break
+    end
+    new_pos = [pos[0] + DIRS[dir][0], pos[1] + DIRS[dir][1]]
+    if new_pos == obst || obstacles[new_pos]
+      dir = (dir + 1) % 4
+    else
+      visited[(pos + [dir])] = true
+      pos = new_pos
+    end
+  end
+end
 
 # MemoryProfiler.stop.pretty_print
 helper.output(a, b)
