dad93bff7fbd81d7a212402d8f1bff6a0c9f4e11
Manu Meixell <misternu@gmail.com>
2024-12-07 12:58:03 -0800

    Depth first recursive sped up solution 2024 day 7

--- 2024/07/solution.rb
+++ 2024/07/solution.rb
@@ -14,14 +14,13 @@ sample_input = helper.auto_parse('sample_input.txt')
 input = input.map { |l| l.map(&:to_i) }
 
 # Part 1
-def values(numbers, part_two = false)
+def values(numbers)
   return numbers if numbers.length == 1
 
   last = numbers[-1]
-  front = values(numbers[...-1], part_two)
+  front = values(numbers[...-1])
   result = front.map { |v| last + v }
   result += front.map { |v| last * v }
-  result += front.map { |v| (v.to_s + last.to_s).to_i } if part_two
   result
 end
 
@@ -35,12 +34,38 @@ end
 
 # Part 2
 
+def search(target, numbers, val, i)
+  if i == numbers.length
+    return val == target ? target : 0
+  end
+
+  concat = (val.to_s + numbers[i].to_s).to_i
+  if concat <= target
+    result = search(target, numbers, concat, i + 1)
+    return result if result > 0
+  end
+
+  product = val * numbers[i]
+  if product <= target
+    result = search(target, numbers, product, i + 1)
+    return result if result > 0
+  end
+
+  sum = val + numbers[i]
+  if sum <= target
+    result = search(target, numbers, sum, i + 1)
+    return result if result > 0
+  end
+
+  0
+end
+
 b = 0
 
 input.each do |line|
   target = line[0]
   numbers = line[1..]
-  b += target if values(numbers, true).include?(target)
+  b += search(target, numbers, numbers[0], 1)
 end
 
 # MemoryProfiler.stop.pretty_print
